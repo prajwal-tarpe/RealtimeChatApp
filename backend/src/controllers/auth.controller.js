b31a849410a54f059acd5f86dc2bece726be9279
Prajwal Tarpe <prajwaltarpe8@gmail.com>
2025-07-08 01:31:13 +0530

    Handled typos

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -91,9 +91,9 @@ export const login = async (req, res) => {
 export const logout = (req, res) => {
     try {
         res.cookie("jwt", "", {maxAge:0})
-        res.status(300).json({message: "Logged out successfully!"});
+        res.status(200).json({message: "Logged out successfully!"});
     } catch (error) {
-        console.llg("Error in logout controller", error.message);
+        console.log("Error in logout controller", error.message);
         res.status(500).json({message: "Internal Server Error"});
     }
 };
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,14 @@ import NavBar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
-// import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
 import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
 import {Loader} from "lucide-react";
 import { Toaster } from "react-hot-toast";
-import { useThemeStore } from "./store/useThemeStore";
 
 function App() {
   const {authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
-  const { theme } = useThemeStore();
 
   console.log({onlineUsers});
 
@@ -29,13 +26,12 @@ function App() {
   }
   return (
     <>
-      <div data-theme={theme}>
+      <div>
         <NavBar/>
         <Routes>
           <Route path="/" element={authUser?<HomePage/>:<Navigate to="/login"/>}/>
           <Route path="/signup" element={!authUser?<SignupPage/>:<Navigate to="/"/>}/>
           <Route path="/login" element={!authUser?<LoginPage/>:<Navigate to="/"/>}/>
-          {/* <Route path="/settings" element={<SettingsPage/>}/> */}
           <Route path="/profile" element={authUser?<ProfilePage/>:<Navigate to="/login"/>}/>
         </Routes>
         <Toaster/>
